13461a315b5de0259cdd3beb9b3d127a7bd987e1
saralune <sarahlef59@hotmail.fr>
2024-11-10 22:19:10 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Sara Lefort on 10/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
